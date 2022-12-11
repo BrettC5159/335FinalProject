507740b5f33cf4ac14fb16e958fee952d170f6a5
Brett <brettcap17@gmail.com>
2022-12-11 15:26:52 -0500

    Get top songs

--- server.js
+++ server.js
@@ -2,15 +2,17 @@ const http = require('http');
 const express = require('express');
 const app = express();
 const path = require('path');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const axios = require('axios');
 const querystring = require('querystring');
 const cors = require('cors');
 const ejs = require('ejs');
 const cookieParser = require('cookie-parser');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const { resolveSoa } = require('dns');
 const portNumber = 5000;
-const redirect_uri = `http://localhost:5000/callback`;
 const stateKey = 'spotify_auth_state';
+const spotify_uri = 'https://api.spotify.com/v1/';
 
 require('dotenv').config({path: path.resolve(__dirname, '.env')});
 const username = process.env.MONGO_DB_USERNAME;
@@ -18,6 +20,11 @@ const password = process.env.MONGO_DB_PASSWORD;
 const database = process.env.MONGO_DB_NAME;
 const mongoCollection = process.env.MONGO_COLLECTION;
 const spotifyClientId = process.env.SPOTIFY_CLIENT_ID;
+const redirect_uri = process.env.SPOTIFY_REDIRECT_URI;
+const redirect_uri_gather_songs = process.env.SPOTIFY_REDIRECT_URI_GATHER;
+const base64_auth = process.env.BASE64_AUTH;
+let period;
+let accesstoken;
 
 // Setup web server
 const webServer = http.createServer();
@@ -80,16 +87,17 @@ app.get('/', (req, res) => {
 });
 
 app.use(cookieParser())
-  .use(cors());
+  .use(cors())
+  .use(express.static(__dirname + '/templates'));
 
 app.post('/login', (req, res) => {
-  let period = req.body.period;
+  period = req.body.period;
   console.log(period);
 
   let state = getRandomString(16);
   res.cookie(stateKey, state);
 
-  const scope = 'user-read-private user-read-email';
+  const scope = 'user-top-read';
   res.redirect('https://accounts.spotify.com/authorize?' + 
     querystring.stringify({
       response_type: 'code',
@@ -101,6 +109,38 @@ app.post('/login', (req, res) => {
 
 });
 
-app.get('/callback', (req, res) => {
-  res.end('Logged in!');
+app.get('/callback', async (req, res) => {
+  console.log('Doing some authorization and setup...');
+
+  const spotifyResponse = await axios.post(
+    'https://accounts.spotify.com/api/token',
+    querystring.stringify({
+      grant_type: "authorization_code",
+      code: req.query.code,
+      redirect_uri: redirect_uri
+    }),
+    {
+      headers: {
+        Authorization: "Basic " + base64_auth,
+        "Content-Type": "application/x-www-form-urlencoded"
+      }
+    }
+  );
+
+  accesstoken = spotifyResponse.data.access_token;
+  res.redirect('/gathersongs')
+});
+
+app.get('/gathersongs', async (req, res) => {
+  console.log('Getting your top songs...')
+  const topSongsRawData = await axios.get(
+    "https://api.spotify.com/v1/me/top/tracks?limit=50", 
+    {
+      headers: {
+        Authorization: "Bearer " + accesstoken,
+        'Content-Type': 'application/json'
+    }});
+
+    
+
 });
